770a7e5d5c461f7116ad88a70e91152f463b75a3
池田快斗 <kaikai0627tomato@au.com>
2026-09-05 18:46:18 +0900

    reactの練習。カートの追加及び削除、～の倍数になったらコンソールに報告

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
 import "./App.css"
-import Profile from "./compornents/button"
 
 function App() {
 
-    const myname = "山田太郎"
-    const age = 20
-    const job = "大学生"
-    const birthplace = "愛知県"
-    const hobby = "ゲーム"
-    const favoriteFood = "ラーメン"
-    const favoriteSport = "サッカー"
-    const favoriteColor = "青"
-    const email = "example@example.com"
-    const introduction = "よろしくお願いします！"
+    const [count, setCount] = useState([
+        { id: 1, name: "商品A", cart: false },
+        { id: 2, name: "商品B", cart: false },
+        { id: 3, name: "商品C", cart: false },
+        { id: 4, name: "商品D", cart: false },
+        { id: 5, name: "商品E", cart: false },
+        { id: 6, name: "商品F", cart: false },
+    ])
+
+    const handleClick = (id) => 
+        setCount((prev) => 
+            prev.map((product) => 
+                product.id === id ? {
+                    ...product, cart: !product.cart
+                } : product
+            )
+        )
+// useEffectで何がしたい＝count検証によるコンソール通信
+// cartを数にしないといけない
+    useEffect(() => {
+        const cartTrue = count.filter(product => product.cart).length
+
+        if (cartTrue !== 0 && cartTrue % 3 ===0 ) {
+            console.log(`現在のカートの数は${cartTrue}です！`)
+        }
+    },[count])
 
     return (
-        <BrowserRouter>
-            <Routes>
-                {/* Ctrl + Alt + PgDn複数編集 */}
-                <Profile
-                myname={myname}
-                age={age}
-                job={job}
-                birthplace={birthplace}
-                hobby={hobby}
-                favoriteFood={favoriteFood}
-                favoriteSport={favoriteSport}
-                favoriteColor={favoriteColor}
-                email={email}
-                introduction={introduction}
-                />
-            </Routes>
-        </BrowserRouter>
+        <>
+            {count.map((product) => (
+                <div key={product.id}>
+                    <p>{product.name}</p>
+                    <button type="button" onClick={() => handleClick(product.id)}>
+                        {product.cart ? "カートに削除":"カートから追加"}
+                    </button>
+                </div>
+            )
+        )}
+        </>
     )
 };
+
 export default App;
