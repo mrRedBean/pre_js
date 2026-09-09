d9c83544d0aaee33c2c2b45c1c20bb81cbed0c46
池田快斗 <kaikai0627tomato@au.com>
2026-09-09 20:47:58 +0900

    2026/09/09 reactの復習

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,55 @@
 import { useEffect, useState } from "react"
 import "./App.css"
-
-function App() {
-
-    const [count, setCount] = useState([
-        { id: 1, name: "商品A", cart: false },
-        { id: 2, name: "商品B", cart: false },
-        { id: 3, name: "商品C", cart: false },
-        { id: 4, name: "商品D", cart: false },
-        { id: 5, name: "商品E", cart: false },
-        { id: 6, name: "商品F", cart: false },
+function App () {
+    const [count,setCount] = useState([
+        {id:1,name:"A",bought:false},
+        {id:2,name:"B",bought:false},
+        {id:3,name:"C",bought:false},
+        {id:4,name:"D",bought:false},
+        {id:5,name:"E",bought:false},
+        {id:6,name:"F",bought:false},
+        {id:7,name:"G",bought:false},
+        {id:8,name:"H",bought:false},
+        {id:9,name:"I",bought:false},
+        {id:10,name:"J",bought:false},
+        {id:11,name:"K",bought:false},
+        {id:12,name:"L",bought:false},
+        {id:13,name:"M",bought:false},
+        {id:14,name:"N",bought:false},
+        {id:15,name:"O",bought:false},
+        {id:16,name:"P",bought:false},
     ])
 
-    const handleClick = (id) => 
-        setCount((prev) => 
-            prev.map((product) => 
-                product.id === id ? {
-                    ...product, cart: !product.cart
-                } : product
-            )
-        )
-// useEffectで何がしたい＝count検証によるコンソール通信
-// cartを数にしないといけない
-    useEffect(() => {
-        const cartTrue = count.filter(product => product.cart).length
+    
+    const handleClick = (id) => {
+        setCount((prev) =>
+            prev.map((product) =>
+                product.id === id ?
+        {...product, bought: !product.bought} : product
+    )
+)
+}
+useEffect (() => {
+    const boughtTrue = count.filter(product => product.bought).length
 
-        if (cartTrue !== 0 && cartTrue % 3 ===0 ) {
-            console.log(`現在のカートの数は${cartTrue}です！`)
-        }
-    },[count])
-
+    if (boughtTrue !== 0 && boughtTrue % 3 === 0) {
+        console.log(`現在の購入数は${boughtTrue}です！`)
+    }
+},[count])
     return (
         <>
-            {count.map((product) => (
+        {
+            count.map((product)=> (
                 <div key={product.id}>
                     <p>{product.name}</p>
                     <button type="button" onClick={() => handleClick(product.id)}>
-                        {product.cart ? "カートに削除":"カートから追加"}
+                        <p>{product.bought ? "購入取り消し":"この商品をカートに入れる"}</p>
                     </button>
                 </div>
-            )
-        )}
+            ))
+        }
         </>
     )
-};
-
+}
+ 
 export default App;
